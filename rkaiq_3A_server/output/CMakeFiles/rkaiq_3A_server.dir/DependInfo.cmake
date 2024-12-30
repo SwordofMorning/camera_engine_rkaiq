9301ee50158f09ef52878ed91035536d9127b381
xjt <969211735@qq.com>
2024-12-30 14:45:13 +0800

    first

---
+++ rkaiq_3A_server/output/CMakeFiles/rkaiq_3A_server.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/xjt/_Workspace_/VOC/camera_engine_rkaiq/rkaiq_3A_server/common/mediactl/mediactl.c" "/home/xjt/_Workspace_/VOC/camera_engine_rkaiq/rkaiq_3A_server/output/CMakeFiles/rkaiq_3A_server.dir/common/mediactl/mediactl.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_CRT_SECURE_NO_WARNINGS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include/common/mediactl"
+  "../."
+  ".././common"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xjt/_Workspace_/VOC/camera_engine_rkaiq/rkaiq_3A_server/rkaiq_3A_server.cpp" "/home/xjt/_Workspace_/VOC/camera_engine_rkaiq/rkaiq_3A_server/output/CMakeFiles/rkaiq_3A_server.dir/rkaiq_3A_server.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_CRT_SECURE_NO_WARNINGS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include/common/mediactl"
+  "../."
+  ".././common"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ rkaiq_3A_server/output/CMakeFiles/rkaiq_3A_server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rkaiq_3A_server.dir/rkaiq_3A_server.cpp.o"
+  "CMakeFiles/rkaiq_3A_server.dir/common/mediactl/mediactl.c.o"
+  "rkaiq_3A_server.pdb"
+  "rkaiq_3A_server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/rkaiq_3A_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
